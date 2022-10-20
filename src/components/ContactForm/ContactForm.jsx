ee6ae8b98672eba217e8d427c5b24a76cba76f29
syniepolskyi <syniepolskyi@yahoo.com>
2022-10-20 21:36:58 +0300

    Changed input pattern and title for name

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -23,6 +23,8 @@ export class ContactForm extends React.Component {
             type="text"
             id={this.inputNameId}
             name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             value={name}
             onChange={handleChange}
             required
